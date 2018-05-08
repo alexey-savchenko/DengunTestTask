5f0dfd10bcb4d61ff456c5889c0c07fb658d4cb3
Alexey Savchenko <a.savchenko@aboutcode.us>
2018-05-08 18:37:09 +0300

    Cleared code

---
+++ DengunTestTask/UserHomeController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  DengunTestTask
+//
+//  Created by Alexey Savchenko on 08.05.2018.
+//  Copyright © 2018 Alexey Savchenko. All rights reserved.
+//
+
+import UIKit
+
+class UserHomeController: UIViewController {
+  
+}
