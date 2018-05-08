f776373d7a858b253a2f6a292c541aaff900d293
Alexey Savchenko <a.savchenko@aboutcode.us>
2018-05-08 18:06:32 +0300

    Initial commit.

---
+++ DengunTestTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DengunTestTask
+//
+//  Created by Alexey Savchenko on 08.05.2018.
+//  Copyright © 2018 Alexey Savchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
